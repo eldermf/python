e2fecb97ca944857c18082578296b254ba7657b9
eldermf <eldermf@bsd.com.br>
2025-05-09 13:35:26 -0300

    App with password

---
+++ app.py
@@ -0,0 +1,44 @@
+import customtkinter as ctk 
+
+# Criação das funcionalidades
+def validar_login():
+    usuario = campo_usuario.get()
+    senha = campo_senha.get()
+
+    # Verificar se o usuário é eldermf e a senha vamula
+    if usuario == 'eldermf' and senha == 'vamula':
+        resultado_login.configure(text='Login feito com sucesso!',text_color='green')
+    else:
+        resultado_login.configure(text='Login incorreto',text_color='red')
+
+# Configuração da aparência
+ctk.set_appearance_mode('dark')
+
+# Criação da janela principal
+app = ctk.CTk()
+app.title('Sistema de Login')
+app.geometry('300x300')
+
+# Criação dos campos
+# Label
+label_usuario = ctk.CTkLabel(app,text='Usuário')
+label_usuario.pack(pady=10)
+# Entry
+campo_usuario = ctk.CTkEntry(app,placeholder_text='Digite seu usuário')
+campo_usuario.pack(pady=10)
+
+# Label
+label_senha = ctk.CTkLabel(app,text='Senha')
+label_senha.pack(pady=10)
+# Entry
+campo_senha = ctk.CTkEntry(app,placeholder_text='Digite sua senha',show='*')
+campo_senha.pack(pady=10)
+# Button
+botao_login = ctk.CTkButton(app,text='Login',command=validar_login)
+botao_login.pack(pady=10)
+# Campo feedback de login
+resultado_login = ctk.CTkLabel(app,text='')
+resultado_login.pack(pady=10)
+
+# Iniciar a aplicação
+app.mainloop()
